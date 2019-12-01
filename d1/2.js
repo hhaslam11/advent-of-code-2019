82cb63f3ee29ab404850e0c7bb163eb388c4729d
hhaslam11 <kalebmarc@hotmail.com>
2019-12-01 13:07:18 -0800

    clean up code a bit

--- d1/2.js
+++ d1/2.js
@@ -2,14 +2,16 @@ const fs = require('fs');
 
 fs.readFile('./input.txt', 'utf8', (e, val) => {
   const ar = val.split('\n');
+  
   let total = 0;
   ar.forEach(el => {
     total += getFuel(el);
   });
+
   console.log(total);
 });
 
-const getFuel = (mass) => {
+const getFuel = mass => {
   const calc = Math.floor(mass / 3) - 2;
   if (calc <= 0) return 0;
 
