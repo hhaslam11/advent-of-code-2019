c7b3f366df84c670f0a5a0fb87344bcfc06a95c9
hhaslam11 <kalebmarc@hotmail.com>
2019-12-01 13:19:00 -0800

    prep for day 2

---
+++ d2/1.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+fs.readFile('./input.txt', 'utf8', (e, val) => {
+  const ar = val.split('\n');
+  let total = 0;
+
+  ar.forEach(el => {
+    
+    
+  });
+
+  console.log(total);
+});
