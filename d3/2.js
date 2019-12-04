b746931b78a3dffbe28468f4d3e8b0dcd6bfb34b
hhaslam11 <kalebmarc@hotmail.com>
2019-12-03 20:51:29 -0800

    clean up day 3 part 2 code

--- d3/2.js
+++ d3/2.js
@@ -4,96 +4,76 @@ fs.readFile('./input.txt', 'utf8', (e, val) => {
   console.log(calc(val));
 });
 
-const calc = (x) => {
-  let [w1, w2] = x.split('\n');
-  w1 = w1.split(',');
-  w2 = w2.split(',');
+const calc = x => {
+  let [wire1, wire2] = x.split('\n');
+  wire1 = wire1.split(',');
+  wire2 = wire2.split(',');
 
-  let c1 = [];
-  let c1x = 0;
-  let c1y = 0;
+  let coords1 = [];
+  let coords1x = 0;
+  let coords1y = 0;
 
-  let c2 = [];
-  let c2x = 0;
-  let c2y = 0;
+  let coords2 = [];
+  let coords2x = 0;
+  let coords2y = 0;
 
-  let steps = 0;
+  let stepsTaken = 0;
 
-  for (let i = 0; i < w1.length; i++) {
-    let dir = w1[i][0];
-    let count = parseInt(w1[i].slice(1));
+  for (let i = 0; i < wire1.length; i++) {
+    let direction = wire1[i][0];
+    let count = parseInt(wire1[i].slice(1));
 
-    if (dir === 'U') {
-      for (let j = 0; j < parseInt(count); j++) {
-        c1y++;
-        steps++;
-        if (!c1[c1x]) c1[c1x] = [];
-        c1[c1x][c1y] = c1[c1x][c1y] || steps;
+    for (let j = 0; j < parseInt(count); j++) {
+      switch (direction) {
+      case 'U':
+        coords1y++;
+        break;
+      case 'D':
+        coords1y--;
+        break;
+      case 'L':
+        coords1x--;
+        break;
+      case 'R':
+        coords1x++;
+        break;
       }
-    } else if (dir === 'D') {
-      for (let j = 0; j < parseInt(count); j++) {
-        c1y--;
-        steps++;
-        if (!c1[c1x]) c1[c1x] = [];
-        c1[c1x][c1y] = c1[c1x][c1y] || steps;
-      }
-    } else if (dir === 'L') {
-      for (let j = 0; j < parseInt(count); j++) {
-        c1x--;
-        steps++;
-        if (!c1[c1x]) c1[c1x] = [];
-        c1[c1x][c1y] = c1[c1x][c1y] || steps;
-      }
-    } else if (dir === 'R') {
-      for (let j = 0; j < parseInt(count); j++) {
-        c1x++;
-        steps++;
-        if (!c1[c1x]) c1[c1x] = [];
-        c1[c1x][c1y] = c1[c1x][c1y] || steps;
-      }
+      stepsTaken++;
+      if (!coords1[coords1x]) coords1[coords1x] = [];
+      coords1[coords1x][coords1y] = coords1[coords1x][coords1y] || stepsTaken;
     }
+    
   }
-  let ar = [];
-
-  steps = 0;
-  for (let i = 0; i < w2.length; i++) {
-    let dir = w2[i][0];
-    let count = parseInt(w2[i].slice(1));
-
-    if (dir === 'U') {
-      for (let j = 0; j < parseInt(count); j++) {
-        steps++;
-        c2y++;
-        if (!c2[c2x]) c2[c2x] = [];
-        if (c1[c2x] && c1[c2x][c2y]) ar.push([c2x, c2y, steps + c1[c2x][c2y]]);
+  
+  const ar = [];
+  stepsTaken = 0;
+  for (let i = 0; i < wire2.length; i++) {
+    let direction = wire2[i][0];
+    let count = parseInt(wire2[i].slice(1));
+    
+    for (let j = 0; j < parseInt(count); j++) {
+      switch (direction) {
+      case 'U':
+        coords2y++;
+        break;
+      case 'D':
+        coords2y--;
+        break;
+      case 'L':
+        coords2x--;
+        break;
+      case 'R':
+        coords2x++;
+        break;
       }
-    } else if (dir === 'D') {
-      for (let j = 0; j < parseInt(count); j++) {
-        steps++;
-        c2y--;
-        if (!c2[c2x]) c2[c2x] = [];
-        if (c1[c2x] && c1[c2x][c2y]) ar.push([c2x, c2y, steps + c1[c2x][c2y]]);
-      }
-    } else if (dir === 'L') {
-      for (let j = 0; j < parseInt(count); j++) {
-        steps++;
-        c2x--;
-        if (!c2[c2x]) c2[c2x] = [];
-        if (c1[c2x] && c1[c2x][c2y]) ar.push([c2x, c2y, steps + c1[c2x][c2y]]);
-      }
-    } else if (dir === 'R') {
-      for (let j = 0; j < parseInt(count); j++) {
-        c2x++;
-        steps++;
-        if (!c2[c2x]) c2[c2x] = [];
-        if (c1[c2x] && c1[c2x][c2y]) ar.push([c2x, c2y, steps + c1[c2x][c2y]]);
-      }
+      stepsTaken++;
+      if (!coords2[coords2x]) coords2[coords2x] = [];
+      if (coords1[coords2x] && coords1[coords2x][coords2y]) ar.push([coords2x, coords2y, stepsTaken + coords1[coords2x][coords2y]]);
     }
-
   }
 
   const sums = [];
-  for (let el of ar) {
+  for (const el of ar) {
     sums.push([Math.abs(el[0]) + Math.abs(el[1]), el[2]]);
   }
   
