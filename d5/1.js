746fb82e3c79dcfeb86c593c7a72357b02ea5781
hhaslam11 <kalebmarc@hotmail.com>
2019-12-04 20:51:06 -0800

    prep day 5

---
+++ d5/1.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.readFile('./input.txt', 'utf8', (e, val) => {
+  console.log(calc(val));
+});
+
+const calc = (x) => {
+  let ar = x.split('\n');
+
+  return ar;
+};
